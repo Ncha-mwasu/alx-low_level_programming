c1878da9ec13b0f46f7fbefabb7c52422b4a299e
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-06-30 07:46:52 -0700

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include<unistd.h>
+#include <unistd.h>
 /**
- * main - print text and gives a standart error
+ * main - Prints text
  *
- * Return: print error 1 (success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str1, 59)
+	write(2, str1, 59);
 	return (1);
 }
