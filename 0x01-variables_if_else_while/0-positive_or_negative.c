858899c8f6552091b09cb7e05aef083751e24b88
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-01 05:17:45 -0700

    positive is better than negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include<stdlib.h>
-#include<time.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
  * main - Determines if a number n is positive, negative or zero.
@@ -17,11 +17,11 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0);
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else (n < 0)
+	else
 	{
 		printf("%d is negative\n", n);
 	}
