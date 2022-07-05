b8da4e3a85bf47f6d89a855128bc5c879fdb0f84
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-05 03:48:04 -0700

    Prints alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/**
+* main - Prints the alphabet a-z using ASCII
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < = 122; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
