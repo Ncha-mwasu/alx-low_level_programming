6733f250e21738a8f4e9f2f3e9acc871248994e0
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-05 03:27:12 -0700

    Prints putchar using putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/**
+* main - Prints putchar with an underscore at the beginning.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, xy;
+
+	xy = sizeof(str) / sizeof(int);
+	for (count = 0; count < xy; count++)
+	{
+		putchar(str[count]);
+	}
+	putchar('\n');
+	return (0);
+}
+
