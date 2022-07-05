91c30eb5dccc67ad93dd888958bd7ee00399a584
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-05 07:36:41 -0700

    Lower case ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints a-z ten times over
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 97; y <= 122; y++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
+	}
+}
