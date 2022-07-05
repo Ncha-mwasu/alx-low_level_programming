cddf4d964a33bf9092b350c8c6b267278f6a244b
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-05 08:10:32 -0700

    Determines the positivity of n

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - Gives i if n is positvie
+ * gives 0 if n is zero and -1 if n is negative
+ *
+ * @n: The input number as an integer.
+ *
+ * Return: 1 is greater than zero. 0 is zero.
+ * -1 is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
