5ac90a5cc2bf8b38148060751841fcdce07aca5a
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-05 06:55:53 -0700

    Lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,11 @@
 *
 * Return: Always 0 (Success)
 */
-
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
