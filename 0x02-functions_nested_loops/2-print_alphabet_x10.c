da87649997cd98d8d576256febb0ecdbad039be9
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-05 04:08:49 -0700

    Prints the alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/**
+ * main - Prints a-z 10 times using ASCII.
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 97; y <= 122; y++)
+		{
+			putchar(y);
+		}
+	putchar('\n');
+	}
+}
