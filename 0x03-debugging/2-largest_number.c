bb421ba213d5f5a49239fe79a3407795a51c8a8c
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-06 16:52:02 -0700

    Debugging Cprog

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,22 +7,22 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
-
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
-	largest = a;
+		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
-	largest = b;
+		largest = b;
 	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
 
 	return (largest);
