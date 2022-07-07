40c94d313c8583324d7b29fac0c070e051b3c076
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-07 01:35:15 -0700

    Letters in upper case return 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * _isupper - checks if characters are letters in upper case.
+ * @c: input character.
+ * Return: 1 if it's an uppercase letter, 0 in other case.
+ */
+int _isupper(int c)
+{
+	int c;
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	{
+	else
+	{
+		return (0);
+	{
+}
