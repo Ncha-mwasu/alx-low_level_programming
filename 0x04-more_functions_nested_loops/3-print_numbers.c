56a3719daaa008a60fb9944445e30a7bf679c93b
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-07-07 02:09:58 -0700

    Prints integers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints integers 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
