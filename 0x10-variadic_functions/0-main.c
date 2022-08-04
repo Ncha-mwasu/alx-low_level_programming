2a9ac53eb0517e38ec99328ebff3b4c00c45eb6b
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-08-04 07:10:25 -0700

    Variable

--- 0x10-variadic_functions/0-main.c
+++ 0x10-variadic_functions/0-main.c
@@ -11,8 +11,8 @@ int main(void)
 	int sum;
 
 	sum = sum_them_all(2, 98, 1024);
-	printf("%d\n", sum );
-	sum = sum_them_all(4, 98, 1024, 403, -1024);
+	printf("%d\n", sum);
+	sum = sum_them_all(4, 98, 1024, 402, -1024);
 	printf("%d\n", sum);
 	return (0);
 }
--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	if(n == 0)
+	if (n == 0)
 		return (0);
 
 	va_start(valist, n);
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
 
 	va_end(valist);
