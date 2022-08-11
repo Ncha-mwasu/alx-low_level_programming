e7d1d6aa915855bbff25efbe1a8830ea2205dc02
Ncha-mwasu <nchamwasuelisha@gmail.com>
2022-08-11 08:50:26 -0700

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * print_list - prints all the elements of a list_t list.
@@ -6,7 +6,7 @@
  *
  * Return: number of elements in the list_t list.
  */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
 	size_t nelem;
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef _LIST_H
-#define _LIST_H
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -16,6 +20,7 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+size_t print_list(const list_t *h);
 void free_list(list_t *head);
 list_t *add_node_end(list_t **head, const char *str);
 size_t list_len(const list_t *h);
