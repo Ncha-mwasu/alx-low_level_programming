8fc27b480719156cadfed52ed0d368f852445c09
Ncha-mwasu <nchamwasuelisha@gmail.com>
2023-05-18 16:51:41 -0700

    C doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,12 +29,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				else
 				{
 					new_node = malloc(sizeof(dlistint_t));
-					if (new_node == NULL)
-					{
-						return (NULL);
-					}
 					new_node->n = n;
 					new_node->next = temp->next;
+					new_node->prev = temp;
 					temp->next->prev = new_node;
 					temp->next = new_node;
 				}
